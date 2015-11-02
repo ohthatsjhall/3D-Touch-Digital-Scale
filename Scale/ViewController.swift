f9d7a2ca257d2d7ea5b38ba81b461e44dc5a29b9
Maxime Defauw <maxime.defauw@gmail.com>
2015-11-02 13:39:30 +0100

    Initial Commit

---
+++ Scale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scale
+//
+//  Created by Maxime Defauw on 02/11/15.
+//  Copyright © 2015 Maxime Defauw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
